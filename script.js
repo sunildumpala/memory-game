41d34d22329c876a6249287d7838982a11babf30
sunildumpala <dumpala@gmail.com>
2023-12-13 14:24:55 -0800

    Preliminary working code

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const COLORS = [
   "purple"
 ];
 
+let selection1 = null;
+let selection2 = null;
+let selectionCount = 0;
+let nomoreClicks = false;
+
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
 // it is based on an algorithm called Fisher Yates if you want ot research more
@@ -51,6 +56,7 @@ function createDivsForColors(colorArray) {
 
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
+    //newDiv.removeEventListener("click", handleCardClick)
 
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
@@ -60,11 +66,76 @@ function createDivsForColors(colorArray) {
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  //console.log("you just clicked", event.target);
   //console.log(event.target.className);
   //console.log(event);
-  event.target.style.backgroundColor = event.target.className;
   
+  
+  //if no clicks allowed end here
+  if(nomoreClicks) return;
+
+  //if selection is already turned, end here
+  if (event.target.classList.contains("turned")) return;
+
+
+  // if none of the above then set the background color of the selection
+  currentSelection = event.target;
+  currentSelection.style.backgroundColor = event.target.className;
+
+
+  // if at least one of the selections is still null
+
+  if (!selection1 || !selection2){
+    currentSelection.classList.add("turned");
+    if (!selection1){
+      selection1 = currentSelection;
+    }
+    else{
+      selection2 = currentSelection;
+    }
+
+  }
+
+  // if neither selection is null
+  if (selection1 && selection2)
+  {
+    nomoreClicks = true;
+
+    /*
+    if selections match
+      increment the selection count
+      remove click event listeners on the selection
+      reset selections
+      reset nomoreClicks
+    otherwise (if no match)
+      reset background colors of both selections
+      reset the classList
+      reset selections
+      reset nomoreClicks
+    */
+
+    if (selection1.className === selection2.className)
+    { 
+      selectionCount += 2;
+      selection1.removeEventListener("click", handleCardClick);
+      selection2.removeEventListener("click", handleCardClick);
+      selection1 = null;
+      selection2 = null;
+      nomoreClicks = false;
+    }else{
+      setTimeout(function(){
+        selection1.style.backgroundColor = "";
+        selection2.style.backgroundColor = "";
+        selection1.classList.remove("turned");
+        selection2.classList.remove("turned");
+        selection1 = null;
+        selection2 = null;
+        nomoreClicks = false;
+      }, 1000);
+    }
+  }
+
+
   
 }
 
